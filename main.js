f44c15b408d014ff2f3900c37caf6020e06e30f1
awaa16 <syalwamadani16@gmail.com>
2025-04-28 09:43:20 +0700

    update link firebase main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
